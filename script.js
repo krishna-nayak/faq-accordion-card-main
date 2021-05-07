a01b9716e56363cdbbcee13769d180a9f192723d
Krishna Nayak <nayak100krishna@gmail.com>
2021-05-07 19:23:52 +0530

    Drop box

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let question = document.querySelectorAll(".question");
+let answer = document.querySelectorAll(".answer");
+let arrowImage = document.querySelectorAll(".arrow img");
+let questionText = document.querySelectorAll(".question-text");
 
+for (let i = 0; i < question.length; i++) {
+    question[i].addEventListener("click", function () {
+        answer[i].classList.toggle("ans");
+        arrowImage[i].classList.toggle("rotate");
+        questionText[i].classList.toggle("text");
+    });
+}
